211ece65a45e7a72c65e855cca26cf299a5441a0
Dmytro Kolchak <dima.kolhac@gmail.com>
2025-10-31 01:32:16 +0200

    Implement solution

--- test.sh
+++ test.sh
@@ -8,8 +8,8 @@ docker network create -d bridge test-network
 docker run --name mysql --network=test-network --hostname mysql  \
 -e MYSQL_ROOT_PASSWORD=P@ssw0rd -v $(pwd):/scripts -d mysql:8.0-debian
 
-echo "INFO: Wainting for database server to initialize"
-sleep 60
+echo "INFO: Waiting for database server to initialize"
+sleep 45
 
 echo "INFO: Creating a database for test"
 docker exec mysql sh -c 'mysql -u root -pP@ssw0rd < /scripts/test-queries/1-create-database.sql'
@@ -41,7 +41,7 @@ docker run --network=test-network -v $(pwd):/repos --workdir /repos/ -e INSTALL_
     -e LIQUIBASE_COMMAND_PASSWORD=P@ssw0rd \
     -e LIQUIBASE_COMMAND_URL=jdbc:mysql://mysql:3306/ShopDB \
     liquibase/liquibase liquibase update --labels="0.0.2"
-
+sleep 10
 echo "INFO: Tagging a database version (0.0.2)"
 docker run --network=test-network -v $(pwd):/repos --workdir /repos/ -e INSTALL_MYSQL=true \
     -e LIQUIBASE_COMMAND_USERNAME=root \
